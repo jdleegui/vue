c39a1a0b8bfa5ff1c240e7bddb77fc23ad9dbe71
jdleegui <jdleegui@gmail.com>
2023-01-08 18:12:56 +0900

    a

--- Vuejs/vue03Webpack/FooterComp.vue
+++ Vuejs/vue03Webpack/FooterComp.vue
@@ -7,7 +7,7 @@ footer {
 </style>
 
 <template>
-  <footer class="jumbotron text-center" style="margin-bottom:0">
+  <footer class="jumbotron text-center" style="margin-bottom: 0">
     <h1>Footer.vue</h1>
   </footer>
 </template>
